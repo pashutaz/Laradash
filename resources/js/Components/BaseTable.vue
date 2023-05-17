65b62f916a11a6daaf399f5eb286534539271d79
Pashutaz <pazaytsev@gmail.com>
2023-05-18 02:11:01 +0400

    Add new BaseTable component
    Signed-off-by: Pashutaz <pazaytsev@gmail.com>

--- resources/js/Components/BaseTable.vue
+++ resources/js/Components/BaseTable.vue
@@ -1,24 +1,3 @@
-<template>
-<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" v-for="column in columns" class="px-6 py-3">
-                    {{ column.name }}
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="row in rows" class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-                <td v-for="column in columns" class="px-6 py-4">
-                    {{ row[column.value] }}
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
-</template>
-
 <script setup lang="ts">
 import {defineProps} from "vue";
 
@@ -28,6 +7,51 @@ defineProps({
 });
 </script>
 
-<style scoped>
+<template>
+    <div id="base_table">
+        <table>
+            <thead>
+                <tr>
+                    <th scope="col" v-for="column in columns">
+                        {{ column.name }}
+                    </th>
+                    <th v-if="$slots['action']"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="row in rows">
+                    <td v-for="column in columns">
+                        {{ row[column.value] }}
+                    </td>
+                    <slot name="action" :row="row" />
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
 
+<style scoped>
+#base_table {
+    @apply relative overflow-x-auto shadow-md sm:rounded-lg;
+}
+
+table {
+    @apply w-full text-sm text-left text-gray-500 dark:text-gray-400;
+}
+
+thead {
+    @apply text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400;
+}
+
+thead th {
+    @apply px-6 py-3;
+}
+
+tbody tr {
+    @apply bg-white border-b dark:bg-gray-900 dark:border-gray-700;
+}
+
+td {
+    @apply px-6 py-4;
+}
 </style>
